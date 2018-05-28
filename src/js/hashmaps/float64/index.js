ea00ae1a52116c1f7a7749a176c0de510702403f
John Doe <cd48b153@gmail.com>
2018-05-28 03:27:59 -0700

    Fixed the NAN/0 keys.
    There are many 64 bit keys that map to NAN or 0:
    
    NAN: [ '-806872:43888814',
      '-471794:-275000838',
      '-239396:675281316',
      '-37771:695288094',
      '-247728:571800569',
      '-507060:937098504',
      '-1016926:-789563825',
      '-779389:535737268',
      '-745707:-891987157',
      '-266029:-919078428',
      '-81313:370878483',
      '-227793:982588381',
      '-433965:-587787242',
      '-519167:868814736',
      '-574662:264323916',
      '-740588:801901793',
      '-921788:-1006518048',
      '-124595:-850751465',
      '-512975:498988118',
      '-269324:-1072206287',
      '-355065:-503050423',
      '-969398:-734049388',
      '-797937:806409779',
      '-252163:246220557',
      '-88614:568749468',
      '-176066:-248617806' ]

--- src/js/hashmaps/float64/index.js
+++ src/js/hashmaps/float64/index.js
@@ -2,23 +2,44 @@ module.exports = class {
     constructor() {
         this._data = new Map;
         this._i32 = new Int32Array(2);
+        this._f64 = new Float64Array(this._i32.buffer);
+        this._nan = {};
     }
 
     get([hi, lo]) {
         this._i32[0] = lo;
         this._i32[1] = hi;
-        return this._data.get(new Float64Array(this._i32.buffer)[0]);
+
+        let key = this._f64[0];
+
+        return Number.isFinite(key) && key != 0 ?
+            this._data.get(key) :
+            this._nan[hi + ':' + lo];
+
     }
 
     set([hi, lo], obj) {
         this._i32[0] = lo;
         this._i32[1] = hi;
-        return this._data.set(new Float64Array(this._i32.buffer)[0]);
+
+        let key = this._f64[0];
+
+        if (Number.isFinite(key) && key != 0)
+            this._data.set(key, obj);
+        else
+            this._nan[hi + ':' + lo] = obj;
     }
 
     *entries() {
         for (const [key, obj] of this._data.entries()) {
-            let [lo, hi] = new Int32Array(key.buffer);
+            this._f64[0] = key;
+            let lo = this._i32[0];
+            let hi = this._i32[1];
+            yield [[hi, lo], obj];
+        }
+
+        for (let [key, obj] of Object.entries(this._nan)) {
+            let [hi, lo] = key.split(':');
             yield [[+hi, +lo], obj];
         }
     }
